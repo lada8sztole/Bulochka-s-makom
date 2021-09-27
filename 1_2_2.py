5ed53235843d1f60dd24b479cb30cc486c23594d
lada8sztole <hvostkometu@gmail.com>
2021-09-27 20:58:50 +0300

    3 sposoba: 1 cheat=2norm

---
+++ 1_2_2.py
@@ -0,0 +1,10 @@
+# Save your first and last name as separate variables, then use string concatenation to add them together
+# with a white space in between and print a greeting.
+name = "Lada "
+surname = "Oblomova"
+print(name+surname)
+
+name = 'Lada'
+print(' '.join([name, surname]))
+
+print(name+' '+surname)
