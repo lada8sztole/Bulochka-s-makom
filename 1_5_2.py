9aba80352eeaa7734327f04fafe789bf2d6aca02
lada8sztole <hvostkometu@gmail.com>
2021-10-23 02:47:48 +0300

    подсчет фруктов

---
+++ 1_5_2.py
@@ -0,0 +1,23 @@
+# Task 2
+#
+# Input data:
+#
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+# Create a function which takes as input two dicts with structure mentioned above,
+# then computes and returns the total price of stock.
+
+s = 0
+for fruit in stock.keys():
+    s += stock[fruit]*prices[fruit]
+print(s)
