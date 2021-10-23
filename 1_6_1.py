20f5d73eb122e19b7b063e8ed1d622be3254620a
lada8sztole <hvostkometu@gmail.com>
2021-10-23 03:30:48 +0300

    random.randint

---
+++ 1_6_1.py
@@ -0,0 +1,10 @@
+# Task 1
+#
+# The Guessing Game.
+#
+# Write a program that generates a random number between 1 and 10 and lets the user guess what number was generated.
+# The result should be sent back to the user via a print statement.
+import random
+b = random.randint(1, 10)
+a = input('try to guess the number ')
+print(b)
